22982710be41f5733f5d5606160389d4a4568d03
zyin <zyin@zyindeMacBook-Pro.local>
2016-10-31 21:52:57 +0800

    refact

--- src/QuickSort.h
+++ src/QuickSort.h
@@ -2,12 +2,12 @@
 #define QUICKSORT_H_
 
 #include <iostream>
-using namespace  std;
+using namespace std;
 
 class QuickSort {
 public:
 
-	void sort(int **array);
+	void sort(int **array, int count);
 
 private:
 	void quickSort(int **array, int leftIndex, int rightIndex);
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
-#include <iostream>
-using namespace std;
+#include "gtest/gtest.h"
 
-//int main() {
-//	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-//	return 0;
-//}
+int main(int argc, char **argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
